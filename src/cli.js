f9105bd90581ba8e195c9b6aaefdad0bffa43426
Thiago Lagden <lagden@gmail.com>
2023-04-19 22:40:54 -0300

    fix unix/linux

--- src/cli.js
+++ src/cli.js
@@ -11,13 +11,13 @@ export async function create(cwd, options) {
 	const cmds = []
 	const others = []
 
-	let win = ''
+	let separator = ';'
 	if (process.platform === 'win32') {
-		win = 'cmd /c chcp 65001>nul'
+		cmds.push('cmd /c chcp 65001>nul')
+		separator = ' && '
 	}
 
 	cmds.push(
-		win,
 		`npx --yes tiged lagden/boilerplate-${options.template}#main ${cwd} --force`,
 		`cd ${cwd}`,
 		'npx --yes tiged lagden/boilerplate-bin/files#main bin --force',
@@ -29,28 +29,16 @@ export async function create(cwd, options) {
 	const fileBaseBack = path.join(cwd, '.conf', 'base.back.sh')
 	let fileBaseToCP
 
-	// const dockerTemplate = path.join(cwd, 'docker-compose.template.yml')
-	// const dockerTemplateFront = path.join(cwd, 'docker-compose.template.front.yml')
-	// const dockerTemplateBack = path.join(cwd, 'docker-compose.template.back.yml')
-	// let dockerTemplateToCP
-
 	if (frontend.has(options.template)) {
 		cmds.push('npx --yes tiged lagden/boilerplate-eslint/files/frontend#main . --force')
 		fileBaseToCP = fileBaseFront
-		// dockerTemplateToCP = dockerTemplateFront
 	} else {
 		cmds.push('npx --yes tiged lagden/boilerplate-eslint/files/backend#main . --force')
 		fileBaseToCP = fileBaseBack
-		// dockerTemplateToCP = dockerTemplateBack
 	}
 
 	if (options.features) {
 		cmds.push('npx --yes tiged lagden/boilerplate-docker-nodejs/files#main . --force')
-		// others.push(
-		// 	copyFile(dockerTemplateToCP, dockerTemplate),
-		// 	rm(dockerTemplateFront, {force: true}),
-		// 	rm(dockerTemplateBack, {force: true}),
-		// )
 	} else if (process.platform === 'win32') {
 		cmds.push(
 			'del /s /q /f .rsync-*',
@@ -72,7 +60,7 @@ export async function create(cwd, options) {
 		cmds.push('rm -rf .github .gitlab-ci.yml')
 	}
 
-	await _exec(cmds.join(' && '))
+	await _exec(cmds.join(separator))
 
 	others.push(
 		copyFile(fileBaseToCP, fileBase),
